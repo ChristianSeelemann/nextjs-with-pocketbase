638246dfa7530722466b61ad3873b28b9a30f665
ChristianSeelemann <chrisstiff@gmx.de>
2022-12-25 05:35:44 +0100

    🔧 Create global fonts and themes for NextUI

---
+++ helpers/globalStyles.ts
@@ -0,0 +1,28 @@
+const globalStyles = {
+  body: {
+    fontFamily: "Bai Jamjuree, sans-serif",
+  },
+  p: {
+    fontFamily: "Bai Jamjuree, sans-serif",
+  },
+  h1: {
+    fontFamily: "Chakra Petch, sans-serif",
+  },
+  h2: {
+    fontFamily: "Chakra Petch, sans-serif",
+  },
+  h3: {
+    fontFamily: "Chakra Petch, sans-serif",
+  },
+  h4: {
+    fontFamily: "Chakra Petch, sans-serif",
+  },
+  h5: {
+    fontFamily: "Chakra Petch, sans-serif",
+  },
+  h6: {
+    fontFamily: "Chakra Petch, sans-serif",
+  },
+};
+
+export default globalStyles;
---
+++ helpers/theme.ts
@@ -0,0 +1,17 @@
+export const themeLight = <any>{
+  type: "light",
+  theme: {},
+  fonts: {
+    primary: "Bai Jamjuree, sans-serif",
+    secondary: "Charaka Petch, sans-serif",
+  },
+};
+
+export const themeDark = <any>{
+  type: "dark",
+  theme: {},
+  fonts: {
+    primary: "Bai Jamjuree, sans-serif",
+    secondary: "Charaka Petch, sans-serif",
+  },
+};
