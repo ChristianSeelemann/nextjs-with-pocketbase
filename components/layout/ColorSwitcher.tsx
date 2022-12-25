449bc0201a875c17433b6c0711b40ddf532cc8ee
ChristianSeelemann <chrisstiff@gmx.de>
2022-12-25 07:48:24 +0100

    ♻️ Refactor Color Switcher component

--- components/layout/ColorSwitcher.tsx
+++ components/layout/ColorSwitcher.tsx
@@ -4,22 +4,33 @@ import { Switch, useTheme } from "@nextui-org/react";
 
 // Import icons
 import { IoMoon, IoSunny } from "react-icons/io5";
+import { useEffect, useState } from "react";
 
 // Fire the component
 export default function ColorSwitcher() {
   const { setTheme } = useNextTheme();
   const { isDark } = useTheme();
 
+  // Set loading state
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Set loading state to false when component is mounted
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  // Return empty switch if loading
+  if (isLoading) return <Switch shadow size="lg" disabled />;
+
+  // Return switch
   return (
-    <>
-      <Switch
-        checked={isDark}
-        shadow
-        size="lg"
-        iconOn={<IoMoon className="text-yellow-100" />}
-        iconOff={<IoSunny className="text-yellow-500" />}
-        onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
-      />
-    </>
+    <Switch
+      checked={isDark}
+      shadow
+      size="lg"
+      iconOn={<IoMoon className="text-yellow-100" />}
+      iconOff={<IoSunny className="text-yellow-500" />}
+      onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+    />
   );
 }
