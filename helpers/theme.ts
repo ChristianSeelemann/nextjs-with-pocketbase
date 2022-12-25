f6572fe327a4fedba2aa50f0c48b0b21deabe9e7
ChristianSeelemann <chrisstiff@gmx.de>
2022-12-25 11:41:36 +0100

    🐛 Fix wrong font placements

--- helpers/theme.ts
+++ helpers/theme.ts
@@ -1,17 +1,21 @@
 export const themeLight = <any>{
   type: "light",
-  theme: {},
-  fonts: {
-    primary: "Bai Jamjuree, sans-serif",
-    secondary: "Charaka Petch, sans-serif",
+  theme: {
+    fonts: {
+      sans: "Bai Jamjuree, sans-serif",
+      primary: "Bai Jamjuree, sans-serif",
+      secondary: "Charaka Petch, sans-serif",
+    },
   },
 };
 
 export const themeDark = <any>{
   type: "dark",
-  theme: {},
-  fonts: {
-    primary: "Bai Jamjuree, sans-serif",
-    secondary: "Charaka Petch, sans-serif",
+  theme: {
+    fonts: {
+      sans: "Bai Jamjuree, sans-serif",
+      primary: "Bai Jamjuree, sans-serif",
+      secondary: "Charaka Petch, sans-serif",
+    },
   },
 };
