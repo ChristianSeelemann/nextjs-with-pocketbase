b4a76a0cc3c909514bdedd131bf75209d7f1131e
ChristianSeelemann <chrisstiff@gmx.de>
2022-12-25 07:34:32 +0100

    ♻️ Refactor color mode switcher component

--- components/layout/ColorSwitcher.tsx
+++ components/layout/ColorSwitcher.tsx
@@ -1,17 +1,25 @@
+// Imoport components
 import { useTheme as useNextTheme } from "next-themes";
 import { Switch, useTheme } from "@nextui-org/react";
 
+// Import icons
+import { IoMoon, IoSunny } from "react-icons/io5";
+
+// Fire the component
 export default function ColorSwitcher() {
   const { setTheme } = useNextTheme();
-  const { isDark, type } = useTheme();
+  const { isDark } = useTheme();
 
   return (
-    <div>
-      The current theme is: {type}
+    <>
       <Switch
         checked={isDark}
+        shadow
+        size="lg"
+        iconOn={<IoMoon className="text-yellow-100" />}
+        iconOff={<IoSunny className="text-yellow-500" />}
         onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
       />
-    </div>
+    </>
   );
 }
