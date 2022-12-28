24b43c51d1bcfffbe22ad71d842121b7cf7867d0
ChristianSeelemann <chrisstiff@gmx.de>
2022-12-28 09:05:53 +0100

    🏷️ Create user type definition

---
+++ types/user.ts
@@ -0,0 +1,20 @@
+export interface authData {
+  id: string;
+  username: string;
+  name: string;
+  avatar: string;
+  avatarUrl: string;
+  banned: boolean;
+  collectionId: string;
+  collectionName: string;
+  created: string;
+  email: string;
+  emailVisibility: boolean;
+  firstName: string;
+  lastName: string;
+  lastActive: string;
+  roles: string[];
+  showOnline: boolean;
+  updated: string;
+  verified: boolean;
+}
