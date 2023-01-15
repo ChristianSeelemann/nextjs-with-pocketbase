4515e37480068ba940a1c790ec9c60c8b0c9c51d
ChristianSeelemann <chrisstiff@gmx.de>
2023-01-16 00:08:29 +0100

    ♻️ Add container to root footer

--- components/footers/RootFooter.tsx
+++ components/footers/RootFooter.tsx
@@ -1,3 +1,9 @@
+import { Container } from "@nextui-org/react";
+
 export default function RootFooter() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <Container xl>Footer</Container>
+    </footer>
+  );
 }
