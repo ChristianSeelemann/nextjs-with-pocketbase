ab34c51832025abaac00e07f5262eb9ad56d532f
ChristianSeelemann <chrisstiff@gmx.de>
2023-01-16 00:09:00 +0100

    ♻️ Change root layout syntax

--- components/layouts/RootLayout.tsx
+++ components/layouts/RootLayout.tsx
@@ -3,11 +3,15 @@ import RootFooter from "../footers/RootFooter";
 import RootHeader from "../headers/RootHeader";
 
 // Fire up the layout
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <main className="mt-[76px] pt-4">
       <RootHeader />
-      <main>{children}</main>
+      {children}
       <RootFooter />
     </main>
   );
